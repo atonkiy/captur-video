c32213c398f510aa151daf0b40ec8d86eb9ec5cf
Andrey Tonkiy <atonkiybrain@gmail.com>
2013-05-27 16:07:05 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 var app = {
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -9,8 +10,11 @@ var app = {
 	captureVideo : function(){
 		var self = this;
 		
+		navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 1});
+		
 		var captureSuccess = function(mediaFiles){
 			var i, len;
+			navigator.notification.alert('test',null,'test');
 			for (i = 0, len = mediaFiles.length; i < len; i += 1) {
 			    uploadFile(mediaFiles[i]);
 			}
@@ -26,23 +30,68 @@ var app = {
 			name = mediaFile.name;
 			navigator.notification.alert(path, null, name);
 		}
-		navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 1});
-		self.showAlert('Capture Video Start', 'Go');
+		
 	},
     initialize: function() {
         var self = this;
 		
-		self.showAlert('Store Initialized', 'Info');
 		$('.capture-video').on('click', $.proxy(this.captureVideo, this));
         //$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
+*/
+
+// Called when capture operation is finished
+    //
+    function captureSuccess(mediaFiles) {
+        var i, len;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+            //uploadFile(mediaFiles[i]);
+			 navigator.notification.alert(mediaFiles.fullPath, null, mediaFile.name);
+        }       
+    }
+
+    // Called if something bad happens.
+    // 
+    function captureError(error) {
+        var msg = 'An error occurred during capture: ' + error.code;
+        navigator.notification.alert(msg, null, 'Uh oh!');
+    }
+
+    // A button will call this function
+    //
+    function captureVideo() {
+        // Launch device video recording application, 
+        // allowing user to capture up to 2 video clips
+        navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 2});
+    }
+
+    // Upload files to server
+    function uploadFile(mediaFile) {
+        var ft = new FileTransfer(),
+            path = mediaFile.fullPath,
+            name = mediaFile.name;
+/*
+        ft.upload(path,
+            "http://my.domain.com/upload.php",
+            function(result) {
+                console.log('Upload success: ' + result.responseCode);
+                console.log(result.bytesSent + ' bytes sent');
+            },
+            function(error) {
+                console.log('Error uploading file ' + path + ': ' + error.code);
+            },
+            { fileName: name });   */
+    }
+
 
 
 document.addEventListener("deviceready", function(){
 // Your javascript phonegap code.
-  app.initialize();
+  $('.capture-video').on('click',function(){
+  	captureVideo();
+  });
 });
 
 
